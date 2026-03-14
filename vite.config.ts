fb5eebf6afa1c8193b5e2250f1573d5e489862bd
YashJ <yashprakashjagtap@yashs-MacBook-Air.local>
2026-03-14 17:01:25 +0530

    Initial portfolio commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Yash-J-portfolio/",   
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +14,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
