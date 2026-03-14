2ff54b8178f6fe8a5c5111ff00c63e79284584f7
Nishant Panjabi <nishant.panjabi23@spit.ac.in>
2026-03-14 20:12:50 +0530

    Updated portfolio changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Yash-J-portfolio/",   
+   base: "/",   
 
   server: {
     host: "::",
